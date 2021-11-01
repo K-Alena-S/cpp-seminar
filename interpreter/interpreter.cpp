3782193b39ab578327ed1593d2b7c1ff52f0e76f
Artemiy Sartakov <artemiy.sartakov@jetbrains.com>
2021-11-01 14:23:52 +0700

    exceptions & cmake

--- interpreter/interpreter.cpp
+++ interpreter/interpreter.cpp
@@ -4,22 +4,40 @@
 #include <vector>
 #include <algorithm>
 #include <array>
+#include <exception>
+
+class interpreter_error: public std::runtime_error {
+ public:
+    interpreter_error(const char * msg): std::runtime_error(msg) {}
+};
 
 class data_it {
  public:    
     uint8_t & operator*() {
         return *_it;
     }
+
+    // куда показывать?
+    // что показывать?
+    // где обрабатывать?
     data_it & operator++() {
         // TODO: _data.end() - fail
+        // 1. assert(_it != _data.end()) : cannot specify output, cannot specify message, cannot handle error
+        // 2. errno / return error code : can specify output, can specify message, can handle error but can easily forget
+        // 3. exceptions
+
+        // std::exception
+        //    - std::runtime_error
+        //    - std::logic_error
+        if (_it == _data.end()) throw interpreter_error("out of bounds: cannot access element after last");
         ++_it;
         return *this; 
     }
 
     data_it & operator--() {
-        // TODO: _data.begin() - fail
+        if (_it == _data.begin()) throw interpreter_error("out of bounds: cannot access element before first");
         --_it;
-        return *this; 
+        return *this;
     }
  private:
     std::array<uint8_t, 100> _data = {};
@@ -116,39 +134,16 @@ Command * Interpreter::get_cmd(std::string::iterator & it, std::string::iterator
     }
 }
 
-// class StrIterator {
-//  public:
-//     StrIterator(std::string & cmds): _cmds(cmds) {}
-//     const char operator*() {
-//         return *_c;
-//     }
-//     StrIterator(std::string & cmds, const char * c): _cmds(cmds), _c(c) {}
-//     bool operator!=(StrIterator & other) {
-//         if (this == &other) return false;
-//         if (&_cmds != &other._cmds) return true;
-//         if (_c != other._c) return true;
-//         return false;
-//     }
-//     StrIterator end() {
-//         // '\0'
-//         const char * last = _cmds.c_str() + (_cmds.size() - 1);
-//         return StrIterator(_cmds, last);
-//     }
-//     StrIterator & operator++() {
-//         _c++;
-//         return *this;
-//     } 
-//  private:
-//     std::string & _cmds;
-//     const char * _c = _cmds.c_str();
-// };
-
 void Interpreter::interpret(std::string & cmds) {
     std::string::iterator it = cmds.begin();
     std::string::iterator end = cmds.end();
     while (it != end) {
         Command * command = get_cmd(it, end);
-        command->apply(_it);
+        try {
+            command->apply(_it);
+        } catch (interpreter_error & e) {
+            std::cout << e.what() << std::endl;
+        }
         it++;
     }
 }
