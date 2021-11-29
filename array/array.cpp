e1d978f77bc9f967f9fba9a1a6d5eee54648cc6e
Artemiy Sartakov <artemiy.sartakov@jetbrains.com>
2021-11-29 14:40:25 +0700

    range method

--- array/array.cpp
+++ array/array.cpp
@@ -1,11 +1,15 @@
 #include "array.hpp"
 #include <cassert>
 #include <algorithm>
+#include <iostream>
 
-int_array::int_array(int capacity) : data_(new int[capacity]), capacity_(capacity) {}
+int_array::int_array(int capacity) : data_(new int[capacity]), capacity_(capacity) {
+    std::cout << "ctor" << std::endl;
+}
 
 int_array::~int_array() {
-    delete[] data_;
+    std::cout << "dtor" << std::endl;
+    if (data_ != nullptr) delete[] data_;
 }
 
 int int_array::push_back(int i) {
@@ -31,6 +35,7 @@ int int_array::size() const {
 }
 
 int_array & int_array::operator=(int_array other) {
+    std::cout << "op=" << std::endl;
     //if (this != &other) {
         //delete[] data_;
         std::swap(data_, other.data_);
@@ -42,8 +47,45 @@ int_array & int_array::operator=(int_array other) {
     return *this;
 }
 
-int_array::int_array(int_array & other) : capacity_(other.capacity_),
+int_array::int_array(const int_array & other) : capacity_(other.capacity_),
                                          size_(other.size_),
                                          data_(new int[other.size_]) {
+    std::cout << "copyctor" << std::endl;
     std::copy(other.data_, other.data_ + size_, data_);
+}
+
+// int_array::stealing_ctor(const int_array & other): capacity_(other.capacity_),
+//                                          size_(other.size_), {
+//     data_ = other.data_;
+//     other.data_ = nullptr;
+//     other.size_ = 0;
+// }
+
+int_array int_array::zero() {
+    static int_array ZERO(0);
+    return ZERO;
+}
+
+// 1, 11 -> 1, 2, 3, ... 10
+// start - inclusive
+// end - exclusive
+int_array range(int start, int end) {
+    assert(end >= start);
+    if (end == start) return int_array::zero();
+    int_array arr(end - start);
+    for (int i = start; i < end; i++) {
+        arr.push_back(i);
+    }
+    return arr;
+}
+
+int main(int argc, char const *argv[]) {
+    // ctor copyctor dtor dtor
+    // int_array arr1;
+    // rvalue, lvalue
+    // int_array iarr = arr1;
+    // some code with arr1
+    int_array iarr = range(1, 11);
+    // here
+    return 0;
 }
--- array/array.hpp
+++ array/array.hpp
@@ -4,7 +4,7 @@
 class int_array {
  public:
     explicit int_array(int capacity);
-    int_array(int_array & other);
+    int_array(const int_array & other);
     ~int_array();
     int_array & operator=(int_array other);
     int push_back(int i);
@@ -13,6 +13,7 @@ class int_array {
     // Assertion error when calling on empty array
     int pop_back();
     int size() const;
+    static int_array zero();
  private:
     int * data_;
     int size_ = 0;
